3ed5eb7306bcc1d3869d687f55ce4b614666da48
2018008068 <aza1200@naver.com>
2022-08-15 16:45:17 +0900

    clean up , use-effect

---
+++ clean-up/src/App.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ use-effect/src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(()=>{
+      console.log("i run only once.")
+  }, [])
+
+  useEffect(()=>{
+      console.log("SEARCH FOR", keyword);
+  },[keyword])
+
+
+  return (
+      <div>
+          <input
+              value={keyword}
+              onChange={onChange}
+              type="text"
+              placeholder="Search here..."/>
+          <h1>{counter}</h1>
+          <button onClick={onClick}>Click me</button>
+      </div>
+  )
+
+}
+
+export default App;
